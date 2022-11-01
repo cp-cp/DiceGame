79f236bc091a5819c5a1d738cbef40f5089f7149
王新宇 <44167593+cp-cp@users.noreply.github.com>
2022-11-01 16:59:17 +0800

    Add files via upload

--- DICE/src/Main.java
+++ DICE/src/Main.java
@@ -5,7 +5,5 @@ public class Main
     public static void main(String[] args)
     {
         Match match=new Match();
-        match.begin();
-        //System.out.println("Please submit two player here:");
     }
 }
